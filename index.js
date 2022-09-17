f72f6a5b7c71d5b870bc9da987ca275ed69719d6
kjeffmarty <112226659+kjeffmarty@users.noreply.github.com>
2022-09-17 15:38:17 -0300

    initifiles

--- index.js
+++ index.js
@@ -12,13 +12,19 @@ app.use(cors({
 
 
 app.get('/', function (req,res) {
-    res.send({"code":50008,"message":"Bad token","data":null})})
+    res.send("code":5{0008,"message":"Bad token","data":null})})
 
  
+ app.post('/api/auth', (req,res) => {
+       res.send("code":5{0008,"message":"Bad token","data":null})})
+    
+    })
+
+
    
     
  app.post('/api/auth/login', (req,res) => {
-       res.send({"code":1,"msg":"OK","time":1663277614,"data":{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbi5uYXNzc2hvcC5jb20iLCJhdWQiOiJhZG1pbi5uYXNzc2hvcC5jb20iLCJpYXQiOjE2NjMyNzc2MTQsIm5iZiI6MTY2MzI3NzYxNCwiZXhwIjoxNjY4NDYxNjE0LCJqdGkiOnsiaWQiOjEyMDI5NywidHlwZSI6Ikg1In19.JfvB44u4CGUl21fHA2nF7KAcnUVgAEmABczuaaaR1oM","userId":120297,"otc_member_level":0}})
+       res.json({"code":1,"msg":"OK","time":1663277614,"data":{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbi5uYXNzc2hvcC5jb20iLCJhdWQiOiJhZG1pbi5uYXNzc2hvcC5jb20iLCJpYXQiOjE2NjMyNzc2MTQsIm5iZiI6MTY2MzI3NzYxNCwiZXhwIjoxNjY4NDYxNjE0LCJqdGkiOnsiaWQiOjEyMDI5NywidHlwZSI6Ikg1In19.JfvB44u4CGUl21fHA2nF7KAcnUVgAEmABczuaaaR1oM","userId":120297,"otc_member_level":0}})
     
     })
     
